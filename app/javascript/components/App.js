68edf53f5ae9106b2c86c9917b251081f655714a
CSapien08 <sapiano08@gmail.com>
2021-08-31 15:32:07 -0700

    Worked on seeds and figureing out the database aspects

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,6 +1,30 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      apartments: []
+    }
+  }
+
+  getApartments = () => {
+    fetch("http://localhost:3000/apartments")
+    .then(response => {
+      return response.json()
+    })
+    .then(apartmentsArray => {
+      // set the state with the data from the backend into the empty array
+      this.setState({ apartments: apartmentsArray })
+    })
+    .catch(errors => {
+      console.log("index errors:", errors)
+    })
+  }
+  componentDidMount(){
+    this.getApartments()
+  }
+
   render() {
     const {
       logged_in,
@@ -9,9 +33,10 @@ class App extends Component {
       sign_in_route,
       sign_out_route
     } = this.props
+
     console.log(current_user)
+    console.log(this.state.apartments)
     return (
-      
       <>
         <h1>Hello World</h1>
         { logged_in &&
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,79 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+users = [
+    {
+        email: 'test@testing.com',
+        password: '123456',
+        password_confirmation: '123456'
+    }
+]
+
+users.each do |att|
+    User.create att
+end
+
+apartments = [
+    {
+        street: '123 Street',
+        city: 'SD',
+        state: 'CA',
+        manager: 'Todd',
+        email: 'Todd@testing.com',
+        price: '1000',
+        bedrooms: 2,
+        bathrooms: 3,
+        pets: 'all pets welcome'
+    },
+    {
+        street: '456 Street',
+        city: 'Oceanside',
+        state: 'CA',
+        manager: 'Jared',
+        email: 'Jared@testing.com',
+        price: '1500',
+        bedrooms: 4,
+        bathrooms: 3,
+        pets: 'No Pets'
+    },
+    {
+        street: '789 Street',
+        city: 'Encinitas',
+        state: 'CA',
+        manager: 'Trey',
+        email: 'trey@testing.com',
+        price: '1000',
+        bedrooms: 2,
+        bathrooms: 3,
+        pets: 'all pets welcome'
+    },
+    {
+        street: '123 Street',
+        city: 'San Marcos',
+        state: 'CA',
+        manager: 'Thomas',
+        email: 'Thomas@testing.com',
+        price: '900',
+        bedrooms: 1,
+        bathrooms: 1.5,
+        pets: 'all pets welcome'
+    },
+    {
+        street: '123456 Street',
+        city: 'Carlsbad',
+        state: 'CA',
+        manager: 'Amanda',
+        email: 'Amanda@testing.com',
+        price: '3000',
+        bedrooms: 5,
+        bathrooms: 1,
+        pets: 'all pets welcome'
+    }
+]
+
+first_user = User.where(email: 'test@testing.com').first
+
+apartments.each do |att|
+    first_user.apartments.create att
+end
