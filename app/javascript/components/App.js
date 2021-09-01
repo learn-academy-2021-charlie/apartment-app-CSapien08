2ac40436764d0fbfd96bc8951dab528c706c5097
CSapien08 <sapiano08@gmail.com>
2021-09-01 14:05:09 -0700

    Worked on getting home page working with a Header and a Footer

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Home from './pages/Home'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 import { 
   BrowserRouter as Router,
@@ -15,54 +17,56 @@ class App extends Component {
     }
   }
 
-  // getApartments = () => {
-  //   fetch("http://localhost:3000/apartments")
-  //   .then(response => {
-  //     return response.json()
-  //   })
-  //   .then(apartmentsArray => {
-  //     // set the state with the data from the backend into the empty array
-  //     this.setState({ apartments: apartmentsArray })
-  //   })
-  //   .catch(errors => {
-  //     console.log("index errors:", errors)
-  //   })
-  // }
-  // componentDidMount(){
-  //   this.getApartments()
-  // }
+  componentDidMount(){
+    this.getApartments()
+  }
 
+  getApartments = () => {
+    fetch("http://localhost:3000/apartments")
+    .then(response => {
+      return response.json()
+    })
+    .then(apartmentsArray => {
+      // set the state with the data from the backend into the empty array
+      this.setState({ apartments: apartmentsArray })
+    })
+    .catch(errors => {
+      console.log("index errors:", errors)
+    })
+  }
+  
   render() {
     const {
       logged_in,
-      current_user,
       new_user_route,
       sign_in_route,
-      sign_out_route
+      sign_out_route,
+      current_user
     } = this.props
-
-    console.log(current_user)
-    //console.log(this.state.apartments)
+    console.log(logged_in)
+    console.log(new_user_route)
     return (
       <>
         <Router>
+          <Header 
+            logged_in={logged_in}
+            sign_in_route={sign_in_route}
+            sign_out_route={sign_out_route}
+            new_user_route={new_user_route}
+          />
           <Switch> 
-            <Route exact path='/' component={ Home } />
+            <Route exact path='/' render={(props) => {
+              return <Home logged_in={logged_in}
+                           sign_in_route={sign_in_route}
+                           sign_out_route={sign_out_route}
+                           new_user_route={new_user_route} 
+                           current_user={current_user}
+                      />
+              }}
+            />
           </Switch>
-        </Router>
-
-        {/* <h1>Hello World</h1>
-        { logged_in &&
-          <div>
-            <a href={sign_out_route}>Sign Out</a>
-          </div>
-        }
-        { !logged_in &&
-          <div>
-            <a href={sign_in_route}>Sign In</a>
-          </div>
-        } */}
-        
+          <Footer />
+        </Router>        
       </>
     );
   }
--- app/javascript/components/pages/Home.js
+++ app/javascript/components/pages/Home.js
@@ -1,10 +1,47 @@
 import React, { Component } from 'react';
+import { Button } from 'reactstrap';
 
 class Home extends Component {
+    
     render() {
+    const {
+        new_user_route,
+        logged_in,
+        sign_in_route,
+        sign_out_route,
+        current_user
+    } = this.props
+    console.log(current_user)
+     
         return (
             <div>
                 <h1>Welcome to Apartment Worlds!</h1>
+                <div className='home-page-nav'>  
+                    <ul>
+                        <a href='/apartmentIndex' className='btn btn-secondary'>Browse Apartments</a>
+                    </ul>
+                    
+                    <ul>
+                        {logged_in &&
+                            <h3>Welcome back {current_user.email} </h3>
+                        }
+                        {!logged_in &&
+                            <a href={new_user_route} className='btn btn-secondary'>Sign Up</a>
+                        }
+                        {!logged_in &&
+                            <a href={sign_in_route} className='btn btn-secondary'>Sign In</a>
+                        }
+                    </ul>
+                </div>
+                
+                
+                {/* {this.props.apartments && this.props.apartments.map((apartment) => {
+                    return (
+                        <div key={apartment.id}>
+                            <h4>{apartment.city}</h4>
+                        </div>
+                    )
+                })} */}
             </div>
         );
     }
